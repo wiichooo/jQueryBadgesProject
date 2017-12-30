c03fe98d20822ac6e09f12b39ccab49089435e99
Luis Godinez <wiichooo@gmail.com>
2017-12-30 00:34:01 -0600

    badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
-
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/wiichooo.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      response.courses.completed.forEach(function(data) {
+          $("#badges").append(
+          "<div class='course'>" 
+          +"<h3>" + data.title + "</h3>"+
+          "<img src='"+data.badge+"'>"+
+          "<a href='"+data.url+"' target='_blank' class='btn btn-primary'>See Course</a>"
+          
+          +"</div>");
+      });
+    }
+  });
 
 });
